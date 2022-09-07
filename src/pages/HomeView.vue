2813d43b8ed544d8e66baf27335ac564df7a72f2
Marcos Piemontez <marcos.piemontez1@gmail.com>
2022-09-07 17:44:41 -0400

    ✨ New Feature: Adicionado novo método de fazer requisição vue3

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,8 +1,14 @@
 <template>
-  <q-page padding class="row flex flex-center">
+  <q-page padding class="row flex flex-center bg-primary text-white">
     <div class="col-12 text-center q-gutter-y-md">
+      <div>
+        <span>Dolar = {{ dolarToday }}</span> <br/>
+        <span>Euro = {{ euroToday }}</span> <br/>
+        <span>Bitcoin = {{ bitcoinToday }}</span> <br/>
+      </div>
+      <hr>
       <div class="row justify-between items-center">
-        <q-select outlined v-model="codeSelect" @input="selectCurrency()" use-input hide-selected fill-input :options="codeOptions">
+        <q-select outlined v-model="codeSelect" @update:model-value="selectCurrency()" use-input hide-selected fill-input :options="codeOptions">
           <template v-slot:no-option>
             <q-item>
               <q-item-section class="text-grey">
@@ -12,7 +18,7 @@
           </template>
         </q-select>
         <span class="text-bold text-h6">Para</span>
-        <q-select outlined v-model="codeInSelect" use-input hide-selected fill-input input-debounce="0" :options="codeInOptions">
+        <q-select outlined v-model="codeInSelect" @update:model-value="selectCurrency()" use-input hide-selected fill-input input-debounce="0" :options="codeInOptions">
           <template v-slot:no-option>
             <q-item>
               <q-item-section class="text-grey">
@@ -30,7 +36,7 @@
 
 <script setup>
   import { ref, onMounted } from 'vue';
-  import currencyService from 'src/services/currencyService.js'
+  import converterService from 'src/composible/converterService'
 
   const codeOptions = ([{ label: 'USD', value: '$' }, { label: 'EUR', value: '€' }, { label: 'BRL', value: 'R$' }]);
   const codeInOptions = ref([{ label: 'USD', value: '$' }, { label: 'EUR', value: '€' }, { label: 'BRL', value: 'R$' }]);
@@ -38,19 +44,68 @@
   const codeInSelect = ref('EUR');
   const firstCurrency = ref('');
   const mondayCurrency = ref('');
+  const currency = ref('');
+  const dolarToday = ref('');
+  const euroToday = ref('');
+  const bitcoinToday = ref('');
 
-  onMounted(() => {
-    selectCurrency();
+  onMounted(async () => {
+    await getDolar();
+    await getEuro();
+    await getBitcoin();
   });
 
   const selectCurrency = () => {
-    if(codeSelect.value && codeInSelect.value) {
-      let queryRequest = `${codeSelect.value}-${codeInSelect.value}`
-      let queryResponse = codeSelect.value + codeInSelect.value
-      currencyService.findByCode(queryRequest).then((response) => {
-        console.log(response.data[`${queryResponse}`]);
+    if(codeSelect.value.label && codeInSelect.value.label) {
+      let queryRequest = `${codeSelect.value.label}-${codeInSelect.value.label}`
+      let queryResponse = queryRequest.replace('-', '');
+      converterService.findByCode(queryRequest).then((response) => {
+        const {data} = response;
+        const currency = data[queryResponse];
       });
     }
+  }
+  const getDolar = async () => {
+    let queryRequest = 'USD-BRL';
+    let queryResponse = queryRequest.replace('-', '');
+    let dolar = '';
+    let currency = [];
+    await converterService.findByCode(queryRequest).then((response) => {
+      const {data} = response;
+      currency = data[queryResponse];
+      dolar = Number(currency['high']);
+      dolarToday.value = dolar;
+    }).catch(() => {
+      console.log('error');
+    });
+  }
+  const getEuro = async () => {
+    let queryRequest = 'EUR-BRL';
+    let queryResponse = queryRequest.replace('-', '');
+    let euro = '';
+    let currency = [];
+    await converterService.findByCode(queryRequest).then((response) => {
+      const {data} = response;
+      currency = data[queryResponse];
+      euro = Number(currency['high']);
+      euroToday.value = euro;
+    }).catch(() => {
+      console.log('error');
+    });
+  }
+  const getBitcoin = async () => {
+    let queryRequest = 'BTC-BRL';
+    let queryResponse = queryRequest.replace('-', '');
+    let bitcoin = '';
+    let currency = [];
+    await converterService.findByCode(queryRequest).then((response) => {
+      const {data} = response;
+      currency = data[queryResponse];
+      bitcoin = Number(currency['high']);
+      bitcoinToday.value = bitcoin;
+    }).catch(() => {
+      console.log('error');
+    });
   }
 
 </script>
